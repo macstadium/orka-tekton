4b76df318e397851077d6e36e022ec0f65a9af3b
Spike Burton <sburton@macstadium.com>
2020-08-18 23:33:41 -0400

    organize repo structure

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM alpine:3.12
 
 RUN apk add --update --no-cache openssh openssl sshpass curl jq
-COPY task.sh /usr/local/bin/tekton-orka
+COPY scripts/task.sh /usr/local/bin/tekton-orka
 RUN chmod 755 /usr/local/bin/tekton-orka
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 kubectl delete task tekton-orka
 kubectl delete taskrun run-tekton-orka
-kubectl apply -f task.yml
-kubectl apply -f taskrun.yml
+kubectl apply -f resources/task.yml
+kubectl apply -f resources/taskrun.yml
 kubectl-tkn taskrun logs run-tekton-orka -f
