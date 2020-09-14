7e81b73de5632ecbd239d0cd9780e4d679f3d28a
Spike Burton <sburton@macstadium.com>
2020-09-13 21:50:41 -0400

    add apply and delete options for install script

--- install-service-account.sh
+++ install-service-account.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 : ${NAMESPACE:="default"}
+: ${ACTION:="apply"}
+
+if [[ "$1" == "-a" || "$1" == "--apply" ]]; then
+  ACTION="apply"
+elif [[ "$1" == "-d" || "$1" = "--delete" ]]; then
+  ACTION="delete"
+fi
 
 sed -e 's/$(namespace)/'"$NAMESPACE"'/' resources/orka-runner.yml.tmpl > resources/orka-runner.yml
-kubectl apply -f resources/orka-runner.yml
+kubectl $ACTION -f resources/orka-runner.yml
 rm -f resources/orka-runner.yml
